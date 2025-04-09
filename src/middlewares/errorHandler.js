ffffb56aafe8b6f330fa31d7b49af0727a0088ec
Waynexlink <wm500972@gmail.com>
2025-04-09 23:44:57 +0100

    final_commit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === development) {
+  if (process.env.NODE_ENV === "development") {
     sendErrDev(err, res);
   } else if (process.env.NODE_ENV === production) {
     sendErrProd(err, res);
